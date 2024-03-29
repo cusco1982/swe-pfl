096b9f411591b1716195751b9e4d3027a2baa328
Julian <cusco2891@gmail.com>
2024-03-29 00:00:20 -0400

    landing

--- app/layout.js
+++ app/layout.js
@@ -13,13 +13,12 @@ export const metadata = {
 };
 
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased tracking-tight bg-white`}>
         <div className="min-h-screen flex flex-col overflow-hidden grow">
-
+          <Navbar />
 
 
           
